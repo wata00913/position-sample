f8753e4bc82d3b3f3dc46aece4e654f744b6bb22
wata00913 <175d8639@gmail.com>
2025-03-15 23:39:12 +0900

    モジュールに分割

---
+++ src/int_position.rs
@@ -0,0 +1,59 @@
+use std::mem;
+use crate::position::Position;
+
+pub struct IntPosition {
+    records: Vec<(String, i32)>,
+}
+
+impl IntPosition {
+    fn new() -> Self {
+        IntPosition { records: vec![] }
+    }
+}
+
+impl Position for IntPosition {
+    fn from_keys(keys: Vec<&str>) -> Self {
+        let mut pos = Self::new();
+        pos.records = keys
+            .iter()
+            .enumerate()
+            .map(|(idx, key)| (key.to_string(), idx as i32))
+            .collect();
+        pos
+    }
+
+    fn add(&mut self, key: &str) {
+        let last = self.records.last();
+        match last {
+            Some(l) => self.records.push((key.to_string(), l.1 + 1)),
+            None => self.records.push((key.to_string(), 1)),
+        }
+    }
+
+    fn insert(&mut self, key: &str, idx: usize) {
+        let updates = &mut self.records[idx..];
+        for r in updates.iter_mut() {
+            r.1 = r.1 + 1;
+        }
+        self.records.insert(idx, (key.to_string(), idx as i32));
+    }
+
+    fn shift(&mut self, from: usize, to: usize) {
+        let t = self.records[to].clone();
+        let f = mem::replace(&mut self.records[from], t);
+        self.records[to] = f;
+    }
+
+    fn delete(&mut self, idx: usize) -> String {
+        let updates = &mut self.records[idx + 1..];
+        for r in updates.iter_mut() {
+            r.1 = r.1 - 1;
+        }
+        self.records.remove(idx).0
+    }
+
+    fn keys(&self) -> Vec<String> {
+        self.records.iter().map(|r| r.0.clone()).collect()
+    }
+}
+
---
+++ src/position.rs
@@ -0,0 +1,46 @@
+pub trait Position {
+    fn from_keys(keys: Vec<&str>) -> Self;
+    fn add(&mut self, key: &str);
+    fn insert(&mut self, key: &str, idx: usize);
+    fn shift(&mut self, from: usize, to: usize);
+    fn delete(&mut self, idx: usize) -> String;
+    fn keys(&self) -> Vec<String>;
+}
+
+pub fn test_from_keys<T: Position>() {
+    let data = vec!["a", "b", "c", "d"];
+    let pos = T::from_keys(data);
+
+    assert_eq!(vec!["a", "b", "c", "d"], pos.keys())
+}
+
+pub fn test_add<T: Position>() {
+    let mut pos = T::from_keys(vec!["a", "b"]);
+    pos.add("c");
+
+    assert_eq!(vec!["a", "b", "c"], pos.keys())
+}
+
+pub fn test_insert<T: Position>() {
+    let mut pos = T::from_keys(vec!["a", "b", "c"]);
+    // a, d, b, c
+    pos.insert("d", 1); // aとbの間に挿入。Resultで結果の判定
+
+    assert_eq!(vec!["a", "d", "b", "c"], pos.keys())
+}
+
+pub fn test_shift<T: Position>() {
+    let mut pos = T::from_keys(vec!["a", "b", "c", "d"]);
+
+    pos.shift(1, 3);
+
+    assert_eq!(vec!["a", "d", "c", "b"], pos.keys())
+}
+
+pub fn test_delete<T: Position>() {
+    let mut pos = T::from_keys(vec!["a", "b", "c", "d"]);
+
+    let deleted = pos.delete(2);
+    assert_eq!("c", deleted);
+    assert_eq!(vec!["a", "b", "d"], pos.keys())
+}
