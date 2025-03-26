300d2d184464b9e8ec27be3b0f32225a0278e7c9
wata00913 <175d8639@gmail.com>
2025-03-26 22:30:18 +0900

    メモとしてコミット

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,42 @@ mod int_position;
 use int_position::IntPosition;
 mod float_position;
 use float_position::FloatPosition;
+mod string_position;
+use string_position::{Point, StringPosition};
 
 fn main() {
-    test_int_position();
-    test_float_position();
+    // let mut f_pos = FloatPosition::new();
+    // for i in 1..=10 {
+    //     f_pos.add(&i.to_string());
+    // }
+    // println!("indices {:?}", f_pos);
+    // println!("order {:?}", f_pos.order());
+    // println!("indices {:?}", f_pos);
+    // let mut f_pos = FloatPosition::from_keys(vec!["a", "z"]);
+    // for i in 0..64 {
+    //     f_pos.insert(&i.to_string(), 1);
+    // }
+    // println!("order {:?}", f_pos.order());
+    // println!("indices {:?}", f_pos);
 }
 
-fn test_float_position() {
-    test_from_keys::<FloatPosition>();
-    test_add::<FloatPosition>();
-    test_insert::<FloatPosition>();
-    test_shift::<FloatPosition>();
-    test_delete::<FloatPosition>();
-}
 
-fn test_int_position() {
-    test_from_keys::<IntPosition>();
-    test_add::<IntPosition>();
-    test_insert::<IntPosition>();
-    test_shift::<IntPosition>();
-    test_delete::<IntPosition>();
+fn sample() {
+    // let mut expected: Vec<String> = ('a'..='z').map(|c| c.to_string()).collect();
+    // let mut keys: Vec<String> = ('b'..='z').map(|c| c.to_string()).collect();
+    // keys.sort_by(|a, b| b.cmp(a));
+
+    // let mut f_pos = FloatPosition::from_keys(vec!["a", "z"]);
+    // for k in keys.iter().map(|k| k.as_str()) {
+    //     f_pos.insert(k, 1);
+    // }
+    // println!("indices: {:?}", f_pos);
+    // println!("keys: {:?}", f_pos.keys());
+    // println!("order: {:?}", f_pos.order());
+    let mut f_pos = FloatPosition::from_keys(vec!["a", "z"]);
+    for i in 0..1060 {
+        f_pos.insert(&i.to_string(), 1);
+    }
+    println!("order {:?}", f_pos.order());
+    println!("indices {:?}", f_pos);
 }
